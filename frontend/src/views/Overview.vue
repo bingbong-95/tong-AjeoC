c07d207565ab695a89005c4f90ab209ad0d6cbf1
jisunpark <jisunpark@hanwha.com>
2020-09-10 17:08:08 +0900

    main1 page -- layout 적용, 데이터 가져오기 기능

--- frontend/src/views/Overview.vue
+++ frontend/src/views/Overview.vue
@@ -1,8 +1,37 @@
 <template>
-  <b-container fluid>
-    <b-row class="step" style=" margin-left: 50px; margin-top: 30px;">데이터 가져오기</b-row>
+  <div style="margin-top: 30px;">
+    <!-- 좌측 Main -->  
+    <div class="leftmain" fluid style=" margin-left: 70px;">
+      <b-row class="step" style="margin-left: 0px;">데이터 가져오기</b-row>
+      <b-form-group style="margin-top: 15px; font-size: 1.1em;">
+        <b-form-radio v-model="loadmethod" value="A" style="margin-bottom: 15px;">Minio에서 데이터 가져오기</b-form-radio>
+        <b-form-radio v-model="loadmethod" value="B">컴퓨터에서 파일 업로드</b-form-radio>
+      </b-form-group>
 
-  </b-container>
+      <!-- 컴퓨터에서 파일 업로드할 경우 -->
+      <div v-if="loadmethod=='B'"> 
+        <b-form-file
+        style="width:500px;"
+        size="sm"
+        v-model="file"
+        :state="Boolean(file)"
+        placeholder="Choose a file..."
+        no-drop="true"
+        accept=".csv"/>
+        <div v-if="file!=null" style="margin-left:435px; margin-top: 5px;">
+          <b-button size="sm" variant="light" class="custombtn">가져오기</b-button>
+        </div>
+      </div>
+    </div>
+
+    <!-- 우측 Main -->
+    <div class="rightmain">
+      <b-row style="font-size:1.2em; font-weight:bolder;">Result</b-row>
+  
+
+    </div>
+  </div>
+
 </template>
 <script>
   // import Vue from 'vue';
@@ -12,15 +41,37 @@
     },
     data() {
       return {
-       
+        loadmethod: '',
+        file: null,
       }
     }
   }
 </script>
 <style>
   .step {
-
+    font-weight: bold;
     font-size: 1.3em;
+  }
+
+  .custombtn {
+    color: #BDBDBD!important;
+    border: 0;
+    outline: 0;
+    background: none;
+  }
+  
+  .coustombtn:hover {
+    color: blue!important;
+  }
+  
+  .leftmain {
+    float:left;
+    width: 600px;
+  }
+
+  .rightmain {
+    float:left;
+    width: 1000px;
   }
 
 </style>
